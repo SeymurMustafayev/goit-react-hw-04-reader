d4dda56ea7438cb525dac657bfc2d299b904a0d6
Seymur <mustafav.seymur@gmail.com>
2019-12-16 16:38:58 +0200

    Updates

---
+++ precache-manifest.03e049af47345bf50c3650e42a0d2a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa66bb4fe61bf7dd4da38f735a2a3793",
+    "url": "/goit-react-hw-04-reader/./index.html"
+  },
+  {
+    "revision": "109f07d54f4ad76672de",
+    "url": "/goit-react-hw-04-reader/./static/css/reader.69693dd8.chunk.css"
+  },
+  {
+    "revision": "a3c4858b658dd07e7618",
+    "url": "/goit-react-hw-04-reader/./static/js/3.92273480.chunk.js"
+  },
+  {
+    "revision": "20ff1006d87775bfd194",
+    "url": "/goit-react-hw-04-reader/./static/js/main.63e2f4da.chunk.js"
+  },
+  {
+    "revision": "109f07d54f4ad76672de",
+    "url": "/goit-react-hw-04-reader/./static/js/reader.58eb9a73.chunk.js"
+  },
+  {
+    "revision": "f1bd25dff4f321619d5b",
+    "url": "/goit-react-hw-04-reader/./static/js/runtime-main.3736dd20.js"
+  }
+]);
